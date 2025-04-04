e8ace71be2b5a534c7085cd4584f969a71f24df5
Doyoon <dlwhdgus482@naver.com>
2025-04-05 02:46:35 +0900

    commit

---
+++ src/component/WeatherBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WeatherBox = () => {
+  return (
+    <div>
+      <div>서울</div>
+      <h2>날씨</h2>
+      <h3>온도</h3>
+    </div>
+  )
+}
+
+export default WeatherBox
